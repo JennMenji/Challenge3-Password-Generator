4e3c7888fca3f397198a96d86b32cc22279cc9a9
Menji <jennifer.menjivar18@gmail.com>
2021-05-12 13:26:34 -0400

    added character arrays and functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,119 @@
+var specialCharacter = [
+  "!",
+  '"',
+  "'",
+  "#",
+  "$",
+  "%",
+  "&",
+  ",",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "\\",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+];
+var uppperCaseCharacter = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCaseCharacter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var randomNumber = function () {
+  var numberCharacter = Math.floor(Math.random() * 10);
+
+  return numberCharacter;
+};
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  console.log(passwordText);
+  //passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Prompt displays when User clicks generate password
+// Prompt for User to select password length from 8-128
+// Prompt for User to select which lowercase, uppercase, numeric, and/or special characters
+// Each inputs should be validated
+// At least one character type should be selected
+// Password should be generated once all prompts are answered that matches the selected criteria
+// Once the password is generated it should written to the page
